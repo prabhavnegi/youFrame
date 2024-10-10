a5c9e894d6c5d58752de1713e0b4399cd748c97c
prabhavnegi <prabhav511@gmail.com>
2024-10-10 22:48:22 +0530

    v1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # youFrame
+Opens your youtube video in a new tab inside an iframe.
+Copy the code locally and add the extension to your chrome browser.
--- popup/popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('openVideo').addEventListener('click', function() {
-    const url = document.getElementById('videoUrl').value;
-    const videoId = new URL(url).searchParams.get('v'); // Extract video ID
-    if (videoId) {
-        chrome.tabs.create({ url: `https://www.youtube.com/embed/${videoId}` });
-    } else {
-        alert('Please enter a valid YouTube video URL.');
-    }
-});
